cc79c7c664a98336beea04b517f41675aecd7f18
Enes Sadık Özbek <es.ozbek@outlook.com>
2022-12-12 17:40:37 +0300

    Fix wrong code generated by ChatGPT

--- src/rnn/RnnTextPredictor.ts
+++ src/rnn/RnnTextPredictor.ts
@@ -34,7 +34,7 @@ export default class RnnTextPredictor {
         tf.layers.bidirectional({
           layer: tf.layers.lstm({units: 128}),
           mergeMode: 'concat',
-          inputShape: [encodedData.input.length],
+          inputShape: [encodedData.length],
         }),
         tf.layers.dense({units: 128, activation: 'relu'}),
         tf.layers.dense({units: 1, activation: 'sigmoid'}),
