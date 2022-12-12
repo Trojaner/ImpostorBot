4e6697cd4211c597b60e0fcacb8879959dcffd8a
Enes Sadık Özbek <es.ozbek@outlook.com>
2022-12-12 17:19:16 +0300

    Let ChatGPT rewrite RNN code from scratch

--- src/rnn/DiscordTextGenerator.ts
+++ src/rnn/DiscordTextGenerator.ts
@@ -165,98 +165,92 @@ async function predictText(
       modelTopology: JSON.parse(dbModel.getDataValue<string>('model_topology')),
       weightSpecs: JSON.parse(dbModel.getDataValue<string>('weight_specs')),
       weightData: buffer,
-      tokenizedData: JSON.parse(dbModel.getDataValue<string>('tokenized_data')),
     };
   }
 
   const rnn = new RnnTextPredictor();
-  if (model) {
-    await rnn.import(model);
-  } else {
-    const entries = await DbMessages.findAll({
-      where: {
-        user_id: userId || {[Op.ne]: null},
-        guild_id: guildId || channel.guild.id,
-        content: {[Op.ne]: ''},
-      },
-    });
+  try {
+    if (model) {
+      await rnn.import(model);
+    } else {
+      const entries = await DbMessages.findAll({
+        where: {
+          user_id: userId || {[Op.ne]: null},
+          guild_id: guildId || channel.guild.id,
+          content: {[Op.ne]: ''},
+        },
+      });
 
-    const commandPrefixes = ['!', '?', '-', '+', '@', '#', '$', '%', '&'];
+      const commandPrefixes = ['!', '?', '-', '+', '@', '#', '$', '%', '&'];
 
-    const messages = entries
-      .map(x => x.getDataValue<string>('content'))
-      .filter(x => x != '' && x)
-      .filter(
-        x =>
-          !commandPrefixes.some(prefix => x.startsWith(prefix) && x.length > 2)
-      );
+      const messages = entries
+        .map(x => x.getDataValue<string>('content'))
+        .filter(x => x != '' && x)
+        .filter(
+          x =>
+            !commandPrefixes.some(
+              prefix => x.startsWith(prefix) && x.length > 2
+            )
+        );
 
-    if (messages.length == 0) {
-      await interaction?.followUp({
+      if (messages.length == 0) {
+        await interaction?.followUp({
+          embeds: [
+            Messages.error().setDescription(
+              'Failed to generate a message. No messages found for this user.'
+            ),
+          ],
+          ephemeral: true,
+        });
+
+        console.error('No messages found');
+        return null;
+      }
+
+      const message = await interaction?.followUp({
         embeds: [
           Messages.error().setDescription(
-            'Failed to generate a message. No messages found for this user.'
+            'User model is not trained or outdated. Training model now, this can take a while...'
           ),
         ],
         ephemeral: true,
       });
 
-      console.error('No messages found');
-      return null;
+      await setTimeout(1000);
+
+      console.log(`Train started with ${messages.length} messages`);
+      await rnn.train(messages);
+
+      console.log('Train finished, saving model...');
+
+      const {modelTopology, weightSpecs, weightData} = await rnn.export();
+
+      await DbModels.create({
+        guild_id: guildId || channel.guild.id,
+        user_id: userId || null,
+        model_topology: JSON.stringify(modelTopology),
+        weight_specs: JSON.stringify(weightSpecs),
+        weight_data: weightData,
+        tokenized_data: 'null',
+        last_message_id: lastKnownMessageId,
+      });
+
+      if (oldModel) {
+        await oldModel.destroy();
+      }
+
+      console.log('Model saved to db');
+      await message?.edit(
+        'Model trained and saved to database. Generating message...'
+      );
     }
 
-    const message = await interaction?.followUp({
-      embeds: [
-        Messages.error().setDescription(
-          'User model is not trained or outdated. Training model now, this can take a while...'
-        ),
-      ],
-      ephemeral: true,
-    });
-
-    await setTimeout(1000);
-
-    console.log(`Train started with ${messages.length} messages`);
-    await rnn.train(messages, {
-      onBatchBegin: async (batch, logs) => {
-        await message?.edit({
-          embeds: [
-            Messages.error().setDescription(
-              `Training model... Batch ${batch} of ${logs.batches}`
-            ),
-          ],
-        });
-      },
-    });
-
-    console.log('Train finished, saving model...');
-
-    const {modelTopology, weightSpecs, weightData, tokenizedData} =
-      await rnn.export();
-
-    await DbModels.create({
-      guild_id: guildId || channel.guild.id,
-      user_id: userId || null,
-      model_topology: JSON.stringify(modelTopology),
-      weight_specs: JSON.stringify(weightSpecs),
-      weight_data: weightData,
-      tokenized_data: JSON.stringify(tokenizedData),
-      last_message_id: lastKnownMessageId,
-    });
-
-    if (oldModel) {
-      await oldModel.destroy();
+    if (input) {
+      return rnn.predictRemainder(input!);
     }
 
-    console.log('Model saved to db');
-    await message?.edit(
-      'Model trained and saved to database. Generating message...'
-    );
+    return rnn.generateRandom();
+  } finally {
+    rnn.dispose();
   }
-
-  if (input) {
-    return rnn.predictRemainder(input!);
-  }
-
-  return rnn.generateRandom();
 }
--- src/rnn/RnnTextPredictor.ts
+++ src/rnn/RnnTextPredictor.ts
@@ -1,6 +1,6 @@
 import * as tf from '@tensorflow/tfjs-node';
 import {io as tfio} from '@tensorflow/tfjs';
-import natural from 'natural';
+import * as tf_tokenizer from 'tf_node_tokenizer';
 
 export type TextMessage = string;
 
@@ -8,89 +8,67 @@ export type ExportedModel = {
   modelTopology: any;
   weightSpecs: tfio.WeightsManifestEntry[];
   weightData: ArrayBuffer;
-  tokenizedData: any;
 };
 
 export default class RnnTextPredictor {
-  tokenizer: natural.WordTokenizer;
-  model: tf.LayersModel | null = null;
-  tokenizedData: any[] | null = null;
+  private model: tf.LayersModel | null = null;
 
-  constructor() {
-    this.tokenizer = new natural.WordTokenizer();
-  }
+  async train(data: TextMessage[]) {
+    this.model = tf.sequential();
+    const biRNN = tf.layers.bidirectional({
+      layer: tf.layers.lstm({units: 128}),
+      mergeMode: 'concat',
+    });
 
-  async train(data: TextMessage[], callbacks?: any) {
-    this.tokenizedData = data
-      .filter(content => content && content.trim() != '')
-      .map(content => this.tokenizer.tokenize(content));
-
-    const encodedData = this.tokenizedData.map(sentence =>
-      sentence.map(word => this.encodeWord(word))
-    );
-
-    const inputTensor = this.encodeBatch(encodedData);
-    const outputTensor = this.encodeBatch(
-      encodedData.map(sentence => sentence.slice(1))
-    );
-
-    this.model = this.createModel();
+    this.model.layers.push(biRNN);
+    this.model.layers.push(tf.layers.dense({units: 128, activation: 'relu'}));
+    this.model.layers.push(tf.layers.dense({units: 1, activation: 'sigmoid'}));
     this.model.compile({
-      loss: 'categoricalCrossentropy',
-      optimizer: 'adamax',
+      optimizer: tf.train.adamax(),
+      loss: 'binaryCrossentropy',
       metrics: ['accuracy'],
     });
 
-    await this.model.fit(inputTensor, outputTensor, {
-      epochs: 3,
-      batchSize: 32,
-      callbacks: {
-        ...callbacks,
-        onBatchBegin: async (batch, logs) => {
-          console.log(`Batch ${batch}: loss = ${logs?.loss || 'N/A'}`);
-
-          if (callbacks?.onBatchBegin) {
-            callbacks?.onBatchBegin(batch, logs);
-          }
-        },
-      },
-    });
+    const tokenizer = new tf_tokenizer.Tokenizer();
+    tokenizer.fitOnTexts(data);
+    const encodedData = tokenizer.textsToSequences(data);
+    const paddedData = tf_tokenizer.padSequences(encodedData, {maxlen: 50});
+    const inputs = tf.tensor2d(paddedData, [
+      paddedData.length,
+      paddedData[0].length,
+    ]);
+    const labels = tf.ones([paddedData.length, 1]);
+    await this.model.fit(inputs, labels, {epochs: 5, batchSize: 32});
   }
 
-  predictRemainder(input: string) {
-    const tokenizedInput = this.tokenizer.tokenize(input);
-    const encodedInput = tokenizedInput.map(word => this.encodeWord(word));
-    const encodedRemainder = this.predictBatch(encodedInput);
-    const decodedRemainder = encodedRemainder.map(wordIdx =>
-      this.decodeWord(wordIdx)
-    );
-    return decodedRemainder.join(' ');
-  }
+  async predictRemainder(input: TextMessage) {
+    if (!this.model) throw new Error('Model not trained yet.');
 
-  predictRelated(input: string) {
-    const tokenizedInput = this.tokenizer.tokenize(input);
-    const encodedInput = tokenizedInput.map(word => this.encodeWord(word));
-    const encodedRemainder = this.predictBatch(encodedInput, true);
-    const decodedRemainder = encodedRemainder.map(wordIdx =>
-      this.decodeWord(wordIdx)
-    );
-    return decodedRemainder.join(' ');
+    const tokenizer = new tf_tokenizer.Tokenizer();
+    const encodedInput = tokenizer.textsToSequences(input);
+    const paddedInput = tf_tokenizer.padSequences(encodedInput, {maxlen: 50});
+    const inputTensor = tf.tensor2d(paddedInput, [
+      paddedInput.length,
+      paddedInput[0].length,
+    ]);
+
+    const prediction = this.model.predict(inputTensor);
+
+    const remainder = tokenizer.sequencesToTexts(prediction);
+    return remainder;
   }
 
   generateRandom() {
-    const encodedSentence = this.predictBatch([], true);
-    const decodedSentence = encodedSentence.map(wordIdx =>
-      this.decodeWord(wordIdx)
-    );
-    return decodedSentence.join(' ');
+    if (!this.model) throw new Error('Model not trained yet.');
+
+    const tokenizer = new tf_tokenizer.Tokenizer();
+    const prediction = this.model.predict(tf.randomNormal([1, 50]));
+    const randomText = tokenizer.sequencesToTexts(prediction);
+    return randomText;
   }
 
   async export(): Promise<ExportedModel> {
-    if (!this.model || !this.tokenizedData) {
-      throw new Error(
-        'Cannot export model: model not trained or imported yet.'
-      );
-    }
+    if (!this.model) throw new Error('Model not trained yet.');
 
     let modelArtifacts: tfio.ModelArtifacts | null = null;
     const modelTopologyType = 'JSON';
@@ -115,94 +93,15 @@ export default class RnnTextPredictor {
       modelTopology: modelArtifacts!.modelTopology!,
       weightSpecs: modelArtifacts!.weightSpecs!,
       weightData: modelArtifacts!.weightData!,
-      tokenizedData: this.tokenizedData,
     };
   }
 
   async import(model: ExportedModel): Promise<void> {
-    this.tokenizedData = model.tokenizedData;
-
     const modelIo = tf.io.fromMemory(model);
     this.model = await tf.loadLayersModel(modelIo);
   }
 
-  private createModel() {
-    const model = tf.sequential();
-    model.add(
-      tf.layers.lstm({
-        units: 32,
-        returnSequences: true,
-        recurrentInitializer: 'glorotNormal',
-        inputShape: [null, this.getVocabularySize()],
-      })
-    );
-    model.add(
-      tf.layers.dense({units: this.getVocabularySize(), activation: 'softmax'})
-    );
-    return model;
-  }
-
-  private encodeBatch(data: number[][]) {
-    const maxLen = Math.max(...data.map(sentence => sentence.length));
-    const paddedData = data.map(sentence => {
-      const padding = Array(maxLen - sentence.length).fill(0);
-      return [...sentence, ...padding];
-    });
-    return tf.tensor2d(paddedData, [data.length, maxLen]);
-  }
-
-  private predictBatch(data: number[], random = false) {
-    if (!this.model) {
-      throw new Error(
-        'Cannot predict batch: Model not trained or imported yet!'
-      );
-    }
-
-    let encodedInput: number[];
-    if (random) {
-      encodedInput = Array(Math.floor(Math.random() * 10)).fill(0);
-    } else {
-      encodedInput = data.slice();
-    }
-
-    let output: number[] = [];
-    for (let i = 0; i < 100; i++) {
-      const inputTensor = this.encodeBatch([encodedInput]);
-      const outputTensor = this.model.predict(inputTensor) as tf.Tensor2D;
-      const prediction = outputTensor.argMax(-1);
-      output = [...output, prediction.dataSync()[0]];
-      encodedInput = [...encodedInput, prediction.dataSync()[0]];
-    }
-
-    return output;
-  }
-
-  private encodeWord(word: string) {
-    const index = this.getVocabulary().indexOf(word);
-    if (index === -1) {
-      return this.getVocabulary().length;
-    }
-    return index;
-  }
-
-  private decodeWord(index: number) {
-    if (index === this.getVocabulary().length) {
-      return '<UNK>';
-    }
-    return this.getVocabulary()[index];
-  }
-
-  private getVocabulary() {
-    if (!this.tokenizedData) {
-      throw new Error(
-        'Cannot get vocabulary: Model not trained or imported yet!'
-      );
-    }
-
-    return [...new Set(this.tokenizedData.flat())];
-  }
-
-  private getVocabularySize() {
-    return this.getVocabulary().length;
+  dispose() {
+    this.model?.dispose();
   }
 }
